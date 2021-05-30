6ba61b5613c1af8c04669b059e7f782673d048d5
rureo <reonakatsuru030623@gmail.com>
2021-05-30 18:29:38 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,98 @@
+class PrototypesController < ApplicationController
+
+before_action :move_to_index, except: [:index, :show,:edit]
+before_action :authenticate_user!,except: [:index, :show]
+
+  
+  
+
+  def index
+    @prototypes=Prototype.all
+
+  end
+
+ 
+
+
+  def new
+    @prototype=Prototype.new
+  end
+
+
+  def edit
+    @prototype=Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype=Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+  end
+end
+
+ 
+
+
+
+  def create
+    @prototype=Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new　　　
+      
+    end
+  end
+
+
+
+  def destroy
+    prototype=Prototype.find(@_params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+
+
+  def show
+    @prototype=Prototype.find(params[:id])
+
+    @comment=Comment.new
+  
+    @comments=@prototype.comments.includes(:user)
+
+  end
+
+
+  
+    private
+    def prototype_params
+      params.require(:prototype).permit(:title,  :catch_copy,  :concept, :image).merge(user_id: current_user.id)
+    end
+  
+
+
+
+
+  
+
+
+
+    def move_to_index
+      unless current_user.id == prototype.user_id 
+        redirect_to action: :index
+      
+      end
+  
+    end
+   
+
+
+
+
+    
+end
+
+    
